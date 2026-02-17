c3075fe90d299b64f23ac9012e26818a6ffd782f
João Messias <joao.2020317758@aluno.iffar.edu.br>
2026-02-17 16:41:25 -0300

    style: Alterção do componente de lista

--- src/Componentes/BarraLateral/itemNavegacao/index.jsx
+++ src/Componentes/BarraLateral/itemNavegacao/index.jsx
@@ -1,14 +1,21 @@
-import { Children } from "react";
 import { styled } from "styled-components"
 
 const ItemDeListaEstilizado = styled.li`
-
+font-size: 24px;
+line-height: 29px;
+margin-bottom: 30px;
+cursor: pointer;
+color:${props => props.$ativo ? '#7B78E5' : '#D9D9D9'};
+display: flex;
+align-items:center;
+gap:22px;
 `
-const ItemNavegacao = ({Children, iconeAtivo, iconeInativo, ativo=false})=>{
+
+const ItemNavegacao = ({children, iconeAtivo, iconeInativo, ativo=false})=>{
     return(
         <ItemDeListaEstilizado>
             <img src={ativo ? iconeAtivo : iconeInativo} alt="Icone do item de navegação" />
-            {Children}
+            {children}
         </ItemDeListaEstilizado>
         )    
 }
