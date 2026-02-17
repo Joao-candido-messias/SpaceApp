8f4dd805c6d73bcd854a933e3dee96368a3642d2
João Messias <joao.2020317758@aluno.iffar.edu.br>
2026-02-17 17:08:05 -0300

    style: Aplicação das fontes e pesos no item de lista

--- src/Componentes/BarraLateral/itemNavegacao/index.jsx
+++ src/Componentes/BarraLateral/itemNavegacao/index.jsx
@@ -1,23 +1,24 @@
 import { styled } from "styled-components"
 
 const ItemDeListaEstilizado = styled.li`
-font-size: 24px;
-line-height: 29px;
-margin-bottom: 30px;
-cursor: pointer;
-color:${props => props.$ativo ? '#7B78E5' : '#D9D9D9'};
-display: flex;
-align-items:center;
-gap:22px;
+  font-size: 24px;
+  line-height: 29px;
+  margin-bottom: 30px;
+  cursor: pointer;
+  color: ${props => props.$ativo ? '#7B78E5' : '#D9D9D9'};
+  display: flex;
+  align-items: center;
+  gap: 22px;
+  font-family: ${props => props.$ativo ? "'GandhiSans-Bold'" : "'GandhiSans-Regular'"};
 `
 
-const ItemNavegacao = ({children, iconeAtivo, iconeInativo, ativo=false})=>{
-    return(
-        <ItemDeListaEstilizado>
-            <img src={ativo ? iconeAtivo : iconeInativo} alt="Icone do item de navegação" />
-            {children}
-        </ItemDeListaEstilizado>
-        )    
+const ItemNavegacao = ({ children, iconeAtivo, iconeInativo, ativo = false }) => {
+  return (
+    <ItemDeListaEstilizado $ativo={ativo}>
+      <img src={ativo ? iconeAtivo : iconeInativo} alt="Ícone do item de navegação" />
+      {children}
+    </ItemDeListaEstilizado>
+  )
 }
 
 export default ItemNavegacao
