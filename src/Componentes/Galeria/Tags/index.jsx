5bbc9f86684ffbc4ff59f0f8eae8cd78342a3006
João Messias <joao.2020317758@aluno.iffar.edu.br>
2026-04-26 14:18:14 -0300

    feat: adiciona navegação por tags com styled-components

---
+++ src/Componentes/Galeria/Tags/index.jsx
@@ -0,0 +1,37 @@
+import {styled} from 'styled-components'
+import tags from './tags.json'
+
+const TagsContainer = styled.div`
+    color:#D9D9D9;
+    display: flex;
+    gap:1em; 
+    margin-top: 2em;
+    font-size: 24px;
+    align-items: center;          
+`
+
+const TagsButton = styled.button`
+    height: 49px;
+    min-width: 77px;
+    padding: 8px 10px;  
+    border-radius: 10px;
+    border: none;
+    background-color:#D9D9D94D;
+    color:#D9D9D9;
+    /* border-image: linear-gradient(
+        to left,
+        #C98CF1,
+        #7B78E5
+    ) 1; */
+`
+
+const Tags = () =>{
+    return(
+        <TagsContainer>
+            <p  >Busque por tags:</p>
+            {tags.map(tag => <TagsButton key={tag.id}>{tag.titulo}</TagsButton>) }
+        </TagsContainer>
+    )
+}
+
+export default Tags 
