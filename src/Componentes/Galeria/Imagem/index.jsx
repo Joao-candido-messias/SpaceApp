6dbd12ac7e3eea90d842c7e527c33f97e77eaee0
João Messias <joao.2020317758@aluno.iffar.edu.br>
2026-04-27 22:32:46 -0300

    feat:Implementa as figures da galeria

--- src/Componentes/Galeria/Imagem/index.jsx
+++ src/Componentes/Galeria/Imagem/index.jsx
@@ -1,17 +1,56 @@
-const Imagem = () =>{
-    return(
-        <figure>
-            <img src="" alt="" />
-            <figcaption>
-                <h3></h3>
-                <footer>
-                    <p>fonte</p>
-                    <button>favorito</button>
-                    <button>Espandir</button>
-                </footer>
-            </figcaption>
-        </figure>
-    )
+import { styled } from "styled-components"
+import BotaoIcone from "../../BotaoIcone"
+
+const Figure = styled.figure`
+    width: ${props => props.$expandida ? '90%' : '460px'};
+    max-width: 100%;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    & > img {
+        max-width: 100%;
+        border-radius: 20px 20px 0 0;
+    }
+    figcaption {
+        background-color: #001634;
+        border-radius: 0px 0px 20px 20px;
+        color: white;
+        box-sizing: border-box;
+        padding: 12px;
+        h4 {
+            flex-grow: 1;
+            font-weight:normal;
+        }
+        h3, h4 {
+            margin: 0;
+            font-size: 16px;
+            font-family: 'GandhiSansBold';
+        }
+    }
+`
+
+const Rodape = styled.footer`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const Imagem = ({ foto, expandida = false, aoZoomSolicitado }) => {
+    return (<Figure $expandida={expandida} id={`foto-${foto.id}`}>
+        <img src={foto.path} alt={foto.alt} />
+        <figcaption>
+            <h3>{foto.titulo}</h3>
+            <Rodape>
+                <h4>{foto.fonte}</h4>
+                <BotaoIcone>
+                    <img src="/icones/favorito.png" alt="Icone de favorito" />
+                </BotaoIcone>
+                {!expandida && <BotaoIcone aria-hidden={expandida} onClick={() => aoZoomSolicitado(foto)}>
+                    <img src="/icones/expandir.png" alt="Icone de expandir" />
+                </BotaoIcone>}
+            </Rodape>
+        </figcaption>
+    </Figure>)
 }
 
 export default Imagem
--- src/Componentes/Galeria/index.jsx
+++ src/Componentes/Galeria/index.jsx
@@ -1,33 +1,43 @@
-import {styled} from "styled-components"
+import { styled } from "styled-components"
 import Titulo from "../Titulo"
 import Tags from "./Tags"
 import Populares from "./Populares"
+import Imagem from "./Imagem"
 
 const GaleriaContainer = styled.div`
     display: flex;
+    gap: 24px;
 `
 
 const SecaoFluida = styled.section`
     flex-grow: 1;
 `
 
-const Galeria = ({fotos = []}) => {
-    return(
+const ImagensContainer = styled.section`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 24px;
+`
+
+const Galeria = ({ fotos = [], aoFotoSelecionada }) => {
+    return (
         <>
-            <Tags/>
+            <Tags />
             <GaleriaContainer>
                 <SecaoFluida>
                     <Titulo>Navegue pela galeria</Titulo>
-                    <ul>
-                        {fotos.map(foto => <li>{foto.titulo}</li>)}
-                    </ul>
-                    
+                    <ImagensContainer>
+                        {fotos.map(foto => <Imagem 
+                            aoZoomSolicitado={aoFotoSelecionada}
+                            key={foto.id} 
+                            foto={foto} />)
+                        }
+                    </ImagensContainer>
                 </SecaoFluida>
-                <Populares/>
-                
+                <Populares />
             </GaleriaContainer>
-             
-        </> 
+        </>
     )
 }
 
